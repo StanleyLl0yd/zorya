4cbecd5212cda48d5d234bbc2552dca61304a4c5
Stanley Lloyd <131160519+StanleyLl0yd@users.noreply.github.com>
2026-09-05 20:26:31 +0200

    Route application startup through platform shell

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,9 @@
 mod app;
 pub mod engine;
+mod platform;
 
 pub use app::{BrowserApp, BrowserModelError, BrowserWindow, BrowserWindowId, Tab, TabId};
+
+pub fn run() -> Result<(), Box<dyn std::error::Error>> {
+    platform::run()
+}
