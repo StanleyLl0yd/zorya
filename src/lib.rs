461d1e31585b69cf4903cd4ff3345872d7714c33
Stanley Lloyd <131160519+StanleyLl0yd@users.noreply.github.com>
2026-09-05 20:10:51 +0200

    Apply rustfmt to public exports

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,3 @@
 mod app;
 
-pub use app::{
-    BrowserApp, BrowserModelError, BrowserWindow, BrowserWindowId, Tab, TabId,
-};
+pub use app::{BrowserApp, BrowserModelError, BrowserWindow, BrowserWindowId, Tab, TabId};
