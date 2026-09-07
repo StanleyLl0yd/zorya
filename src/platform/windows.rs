83eca39e94199a2456ad10d50c7795b3cdc985a2
Stanley Lloyd <131160519+StanleyLl0yd@users.noreply.github.com>
2026-09-07 11:14:57 +0200

    Brand: apply Zorya icon to native Windows shell

--- src/platform/windows.rs
+++ src/platform/windows.rs
@@ -1,4 +1,5 @@
 use super::RunMode;
+use crate::branding::{application_icon_rgba, application_icon_size};
 use crate::async_lifecycle::{
     AsyncRequestSequence, AsyncTarget, CancellationToken, PendingRequest,
 };
@@ -18,7 +19,7 @@ use winit::application::ApplicationHandler;
 use winit::dpi::LogicalSize;
 use winit::event::WindowEvent;
 use winit::event_loop::{ActiveEventLoop, ControlFlow, EventLoop, EventLoopProxy};
-use winit::window::{Window, WindowId};
+use winit::window::{Icon, Window, WindowId};
 
 const START_LOCATION: &str = "about:blank";
 const START_PAGE: &str = include_str!("../../assets/z1-start.html");
@@ -214,8 +215,12 @@ impl NativeShell {
             return Ok(());
         }
 
+        let icon_size = application_icon_size();
+        let icon = Icon::from_rgba(application_icon_rgba(), icon_size, icon_size)
+            .map_err(|error| format!("failed to build Zorya application icon: {error}"))?;
         let attributes = Window::default_attributes()
-            .with_title("Zorya Developer Build")
+            .with_title("Zorya")
+            .with_window_icon(Some(icon))
             .with_inner_size(LogicalSize::new(1100.0, 760.0));
         let window = Arc::new(
             event_loop
